7bfd77d5aa45fd8994fcdd4882bda6fedc848849
dzm2012 <dzm201225@gmail.com>
2016-09-07 19:00:55 -0400

    Removed Example controler and added button to main menu to connect to server

---
+++ Monopoly_Server/Monopoly_Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsynchronousSocketListener temp = new AsynchronousSocketListener();
+      
+        }
+    }
+}
